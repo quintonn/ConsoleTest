bb39e74d6624a449708cdc915bfe7a93e2619ad3
Quintonn <quintonn.rothmann@orionhealth.com>
2014-11-28 13:47:01 +0200

    Created repo

---
+++ ConsoleTestForSpoon/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ConsoleTestForSpoon
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter some text");
+                var input = Console.ReadLine();
+                Console.WriteLine("You entered: " + input);
+                Console.WriteLine();
+                Console.WriteLine("Press any key to continue, or exit to quit");
+                var cmd = Console.ReadLine();
+                if (cmd.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
